2764b57bc11585a52caba28d62ffae13e1f03a88
dmitri lisokonov <dmitri.lisokonov@gmail.com>
2021-12-02 14:01:32 +0100

    added one line fam

--- src/config/ApiConfig.tsx
+++ src/config/ApiConfig.tsx
@@ -1,4 +1,4 @@
 export const apiConfig = {
-    apiUrl: "https://localhost:44352/",
+    apiUrl: "http://localhost:44352/",
     websocketUrl: "ws://192.168.1.2:3002"
 }
--- src/service/WebSocketClient.tsx
+++ src/service/WebSocketClient.tsx
@@ -4,10 +4,12 @@ import { apiConfig } from '../config/ApiConfig';
 import { TraineeInterfaceContext } from '../context/TraineeInterfaceContext';
 import { InfoMessage } from '../model/InfoMessage';
 import { InfoMessageType } from '../model/InfoMessageType';
+import { useNavigate } from 'react-router-dom';
 
 export const WebSocketClient = () => {
     //Public API that will echo messages sent to it back to the client
     const [socketUrl, setSocketUrl] = useState(apiConfig.websocketUrl);
+    let navigate = useNavigate();
     const { completeFlag, setCompleteFlag, flagCompletedUpdate, setFlagCompletedUpdate, team } = useContext(TraineeInterfaceContext);
     const {
         sendMessage,
@@ -29,6 +31,8 @@ export const WebSocketClient = () => {
                     case "SCENARIO STARTED":
                         localStorage.setItem('start', "true");
                         console.log('scenario start');
+                        navigate('../flags');
+
                         break;
                     case "FLAG COMPLETED":
                         console.log('flag complete');
