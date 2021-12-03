57f7efa9e4b69ebe53a90f69961c1f8716455755
dmitri lisokonov <dmitri.lisokonov@gmail.com>
2021-12-03 10:38:09 +0100

    changed url

--- src/service/GuideHttpClient.tsx
+++ src/service/GuideHttpClient.tsx
@@ -1,23 +1,23 @@
 import { apiConfig } from "../config/ApiConfig";
 
 export class GuideHttpClient {
-  public baseUrl: string = apiConfig.url;
+  public baseUrl: string = apiConfig.apiUrl;
 
   public getHints(scenarioId: any) {
     const path = 'hint/';
 
     const headers = {
-        "Content-Type": "application/json",
+      "Content-Type": "application/json",
     }
 
     return new Promise((resolve, reject) => {
-        fetch(this.baseUrl + path + scenarioId, {
-            method: 'GET',
-            headers: headers,
-        })
-            .then((res) => res.json())
-            .then((data) => resolve(data))
-            .catch(reject);
+      fetch(this.baseUrl + path + scenarioId, {
+        method: 'GET',
+        headers: headers,
+      })
+        .then((res) => res.json())
+        .then((data) => resolve(data))
+        .catch(reject);
     });
   }
 
@@ -25,17 +25,17 @@ export class GuideHttpClient {
     const path = 'hint/getflaghint/';
 
     const headers = {
-        "Content-Type": "application/json",
+      "Content-Type": "application/json",
     }
 
     return new Promise((resolve, reject) => {
-        fetch(this.baseUrl + path + teamId + '/' + flagId + '/', {
-            method: 'GET',
-            headers: headers
-        })
-            .then((res) => res.json())
-            .then((data) => resolve(data))
-            .catch(reject);
+      fetch(this.baseUrl + path + teamId + '/' + flagId + '/', {
+        method: 'GET',
+        headers: headers
+      })
+        .then((res) => res.json())
+        .then((data) => resolve(data))
+        .catch(reject);
     });
   }
 
